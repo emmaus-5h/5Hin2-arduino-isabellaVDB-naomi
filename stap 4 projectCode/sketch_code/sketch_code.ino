dd5f082b374b00b706c13b20a3620f8c3aa62884
IsabellaRina <74595066+IsabellaRina@users.noreply.github.com>
2021-11-26 12:11:56 +0100

    Add files via upload

--- stap 4 projectCode/sketch_code/sketch_code.ino
+++ stap 4 projectCode/sketch_code/sketch_code.ino
@@ -8,8 +8,7 @@ void setup() {
   // put your setup code here, to run once:
 // enable console en stuur opstartbericht
   Serial.begin(9600);
-  Serial.println("Test start");
-
+  
   // zet pinmode voor leds
   pinMode(pinLedRood, OUTPUT);
   pinMode(pinLedGeel, OUTPUT);
@@ -19,19 +18,12 @@ void setup() {
   pinMode(pinKnopS1, INPUT);
   pinMode(pinKnopS2, INPUT);
   pinMode(pinKnopStart, INPUT);
-
-  // zet LEDs aan
-  digitalWrite(pinLedRood, HIGH);
-  digitalWrite(pinLedGeel, HIGH);
-  digitalWrite(pinLedGroen, HIGH);
-  // wacht seconde zodat je kunt zien dat de LEDs het doen
-  delay(1000);
-}
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // zet LedA aan/uit afhankelijk van stand van knopA
-  digitalWrite(pinLedRood, HIGH);
+  // zet LEDs aan
+   if (digitalRead(pinKnopStart) == LOW) {
+ digitalWrite(pinLedRood, HIGH);
   delay(1000);
   digitalWrite(pinLedRood, LOW);
   delay(0);
@@ -45,6 +37,15 @@ void loop() {
   delay(1000);
   digitalWrite(pinLedGroen, LOW);
   delay(0);
+  Serial.println("start");
+
+
+  // for loop blz 25
+
+  
   // kleine vertraging, 100 keer per seconde loopen is genoeg
   delay(10);
-}
+  // wacht seconde zodat je kunt zien dat de LEDs het doen
+  
+   };
+};
